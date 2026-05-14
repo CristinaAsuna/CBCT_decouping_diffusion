2e67af2e1e76b9cd073c2573a54e988525d87562
CristinaAsuna <desuki1906@gmail.com>
2026-05-14 12:45:55 +0800

    add ddp

--- sh/train_brach.sh
+++ sh/train_brach.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-#SBATCH -J Branch
+#SBATCH -J Branch_ddp
 #SBATCH -p bme_gpu
 #SBATCH -N 1
 #SBATCH -n 8
 #SBATCH --mem=128G
-#SBATCH --gres=gpu:NVIDIAA10080GBPCIe:1
+#SBATCH --gres=gpu:NVIDIAA100-PCIE-40GB:2
 #SBATCH -t 120:00:00
-#SBATCH -o logs/Branch_%j.out
-#SBATCH -e logs/Branch_%j.err
+#SBATCH -o logs/Branch_base%j.out
+#SBATCH -e logs/Branch_base%j.err
 
 export SWANLAB_MODE="local"
-export SWANLAB_RESUME=must
-export SWANLAB_LOGDIR="/public_bme2/bme-cuizhm/maxquan/Projects/Replicate/Genai/palette/condition_injection/CBCT_decouping_diffusion/swanlog_branch"
+#export SWANLAB_RESUME=must
+export TENSORBOARD_LOGDIR="/public_bme2/bme-cuizhm/maxquan/Projects/Replicate/Genai/palette/consistent/CBCT_decouping_diffusion/tb_branch_baseline"
+
+export SWANLAB_LOGDIR="/public_bme2/bme-cuizhm/maxquan/Projects/Replicate/Genai/palette/consistent/CBCT_decouping_diffusion/swanlog_base"
 source ~/.bashrc
 source /public_bme2/bme-cuizhm/maxquan/Projects/Replicate/Genai/palette/.venv/bin/activate
 
-cd /public_bme2/bme-cuizhm/maxquan/Projects/Replicate/Genai/palette/condition_injection
+cd /public_bme2/bme-cuizhm/maxquan/Projects/Replicate/Genai/palette/consistent
 
-python -m CBCT_decouping_diffusion.train_research \
+torchrun --standalone --nproc_per_node=2 -m CBCT_decouping_diffusion.train_research \
   --config CBCT_decouping_diffusion/configs/full_to_brach.yaml \
-  --resume /public_bme2/bme-cuizhm/maxquan/Projects/Replicate/Genai/palette/condition_injection/CBCT_decouping_diffusion/output/branch/checkpoints/last.pt
+  #--resume /public_bme2/bme-cuizhm/maxquan/Projects/Replicate/Genai/palette/consistent/CBCT_decouping_diffusion/output/branch_baseline/checkpoints/last.pt
